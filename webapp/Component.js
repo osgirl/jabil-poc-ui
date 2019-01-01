dcf32da3301c89937516961b27e713ff36d535af
Raviki023 <ravi23rajendra@gmail.com>
2019-01-01 18:53:53 +0000

    Integration_Commit

--- webapp/Component.js
+++ webapp/Component.js
@@ -20,12 +20,45 @@ sap.ui.define([
 		init: function () {
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
+			var oApplicationModel = new sap.ui.model.json.JSONModel();
 
 			// enable routing
 			// this.getRouter().initialize();
 
 			// set the device model
+			var that = this;
 			this.setModel(models.createDeviceModel(), "device");
+
+			if (this.getComponentData() && this.getComponentData().startupParameters) {
+				var startupParameters = this.getComponentData().startupParameters;
+				if (startupParameters.inboxAPI) {
+					oApplicationModel.setProperty("/runningInInbox", true);
+					oApplicationModel.setProperty("/sApplicationPath", startupParameters.oParameters.sApplicationPath);
+				}
+			}
+
+			// var sUserApiURL = "/services/userapi/attributes?multiValuesAsArrays=true";
+			// // if we are running in Inbox...
+			// // Service-URLs have to be prefixed with application Path
+
+			// sUserApiURL = "/" + oApplicationModel.getProperty("/sApplicationPath") + sUserApiURL;
+
+			// var oUserModel = new sap.ui.model.json.JSONModel(sUserApiURL);
+
+			// oUserModel.attachRequestCompleted(function (oEvent) {
+			// 	if (oEvent.getParameter("success")) {
+			// 		var resultData = oUserModel.getData();
+			// 		if (resultData) {
+			// 			that.userID = resultData.name;
+			// 			var taskUrl = window.location.href;
+			// 			var instanceId = taskUrl.split("'")[3];
+			// 		//	var oController = JabilPoc.Jabil_Poc.controller.approverScreen;
+			// 	//		JabilPoc.Jabil_Poc.controller.approverScreen.prototype.getTaskInstanceData(instanceId, oController);
+
+			// 		}
+			// 	}
+			// });
+
 		}
 	});
 });
--- webapp/controller/approverScreen.controller.js
+++ webapp/controller/approverScreen.controller.js
@@ -1,12 +1,20 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/BusyDialog",
+], function (Controller, BusyDialog) {
 	"use strict";
 
 	return Controller.extend("JabilPoc.Jabil_Poc.controller.approverScreen", {
 
 		onInit: function () {
 			var that = this;
+			this.busy = new BusyDialog();
+
+			this.oHeader = {
+				"Accept": "application/json",
+				"Content-Type": "application/json"
+			};
+
 			var oComponent = this.getOwnerComponent();
 			// this._router = oComponent.getRouter();
 			// this._router.attachRoutePatternMatched(function(oEvent) {
@@ -14,29 +22,60 @@ sap.ui.define([
 			// 	//that.routePatternMatched(oEvent);
 			// }); 
 
-			var oBulkUploadModel = this.getOwnerComponent().getModel("oBulkUploadModel");
-			this.getView().setModel(oBulkUploadModel, "oBulkUploadModel");
+			var oTaskContextDataModel = this.getOwnerComponent().getModel("oTaskContextDataModel");
+			this.getView().setModel(oTaskContextDataModel, "oTaskContextDataModel");
 			var oTblHdrDetailsModel = new sap.ui.model.json.JSONModel();
 			this.getView().setModel(oTblHdrDetailsModel, "oTblHdrDetailsModel");
-			this.oBulkUploadModel = oBulkUploadModel;
+			this.oTaskContextDataModel = oTaskContextDataModel;
 
-			//this.getRecordsList();
+			this.oApproverDataModel = this.getOwnerComponent().getModel("oApproverDataModel");
+			this.oApproverDataModel = this.oApproverDataModel;
+
+			this.getUserDetails();
 		},
-		
-		getAllRecords: function () {
+
+		getUserDetails: function () {
+			var oUserModel = this.getOwnerComponent().getModel("oUserModel");
+			var sUrl = "/html5apps/jabilpocui/services/userapi/attributes?multiValuesAsArrays=true";
+			//	var sUrl = "/services/userapi/currentUser";
+			var oModel = new sap.ui.model.json.JSONModel();
+			var that = this;
+			oModel.loadData(sUrl, true, "GET", false, false);
+			oModel.attachRequestCompleted(function (oEvent) {
+				if (oEvent.getParameter("success")) {
+					var resultData = oEvent.getSource().getData();
+					if (resultData) {
+						oUserModel.setData(resultData);
+						that.userID = resultData.name;
+						var taskUrl = window.location.href;
+						var instanceId = taskUrl.split("'")[3];
+						that.instanceId = instanceId;
+						that.getTaskInstanceData(instanceId);
+					}
+				} else {
+					sap.m.MessageToast.show("Internal Server Error");
+				}
+			});
+			oModel.attachRequestFailed(function (oEvent) {
+				sap.m.MessageToast.show("Internal Server Error");
+			});
+		},
+
+		getTaskInstanceData: function (instanceId) {
 			var that = this;
 			this.busy.open();
-			var oApproverDataModel = this.oApproverDataModel;
-			var sUrl = "/jabilPriceUpdate/POC_JABIL/service.xsjs/getRequestById?requestId=JABILPU082372";
+			var oTaskContextDataModel = this.oTaskContextDataModel;
+			var sUrl = "/html5apps/jabilpocui/destination/bpmworkflowruntime/workflow-service/rest/v1/task-instances/" + instanceId +
+				"/context";
 			var oModel = new sap.ui.model.json.JSONModel();
 			oModel.loadData(sUrl, "", true, "GET", false, false, this.oHeader);
 			oModel.attachRequestCompleted(function (oEvent) {
 				if (oEvent.getParameter("success")) {
 					var resultData = oModel.getData();
 					if (resultData) {
-						
+						that.oTaskContextDataModel.setData(resultData);
+						that.getRecordsList();
 						that.busy.close();
-						oApproverDataModel.refresh();
 					}
 				} else {
 					//			var errorText = that.oResourceModel.getText("INTERNAL_SERVER_ERROR");
@@ -49,8 +88,51 @@ sap.ui.define([
 				//	formatter.toastMessage(errorText);
 				that.busy.close();
 			});
+
 		},
-		
+
+		getRecordsList: function () {
+			var that = this;
+			if (this.userID === "P1940781507") {
+				this.Ids = this.oTaskContextDataModel.getData().reviewer2.ids;
+			} else if (this.userID === "P2000869457") {
+				this.Ids = this.oTaskContextDataModel.getData().reviewer1.ids;
+			} else {
+				this.Ids = this.oTaskContextDataModel.getData().ids;
+			}
+			this.getRecordsData();
+		},
+
+		getRecordsData: function () {
+			var that = this;
+			this.busy.open();
+			var oPayload = {
+				"ids": this.Ids
+			};
+			this.getView().byId("table1").setVisibleRowCount(this.Ids.length);
+			var sUrl = "/html5apps/jabilpocui/jabilPriceUpdate/POC_JABIL/service.xsjs/getPriceMaintById";
+			var oModel = new sap.ui.model.json.JSONModel();
+			oModel.loadData(sUrl, JSON.stringify(oPayload), true, "POST", false, false, this.oHeader);
+			oModel.attachRequestCompleted(function (oEvent) {
+				if (oEvent.getParameter("success")) {
+					var resultData = oModel.getData();
+					if (resultData) {
+						that.oApproverDataModel.getData().listRequestDto = resultData;
+						that.oApproverDataModel.refresh();
+						that.busy.close();
+					}
+				} else {
+					//		var errorText = that.oResourceModel.getText("INTERNAL_SERVER_ERROR");
+					//		formatter.toastMessage(errorText);
+					that.busy.close();
+				}
+			});
+			oModel.attachRequestFailed(function (oEvent) {
+				//	var errorText = that.oResourceModel.getText("INTERNAL_SERVER_ERROR");
+				//	formatter.toastMessage(errorText);
+				that.busy.close();
+			});
+		},
 
 		//works only for csv files
 		onUpload: function (e) {
@@ -129,7 +211,143 @@ sap.ui.define([
 
 		onBack: function () {
 			this._router.navTo("requestScreen");
-		}
+		},
+
+		// onAction: function () {
+		// 	var that = this;
+		// 	this.busy.open();
+		// 	var instanceId = that.instanceId;
+
+		// 	var oPayLoad = {
+		// 		"context": {
+		// 			"reviewer1": {
+		// 				"isApproved": true
+		// 			}
+		// 		},
+		// 		"status": "COMPLETED",
+		// 		"subject": "Price Update Review - P2000869457",
+		// 		"description": "Price Update Review",
+		// 		"recipientUsers": "P2000869457",
+		// 		"recipientGroups": "string",
+		// 		"processor": "P2000869457",
+		// 		"dueDate": "2018-12-31T04:30:08.983Z",
+		// 		"priority": "VERY_HIGH"
+		// 	};
+
+		// 	var sUrl = "/html5apps/jabilpocui/destination/bpmworkflowruntime/workflow-service/rest/v1/task-instances/" + instanceId;
+		// 	var oModel = new sap.ui.model.json.JSONModel();
+		// 	oModel.loadData(sUrl, JSON.stringify(oPayLoad), true, "POST", false, false, this.oHeader);
+		// 	oModel.attachRequestCompleted(function (data) {
+		// 		if (data.getParameter("success")) {
+		// 			var resultData = oModel.getData();
+		// 			if (resultData) {
+		// 				// that.oTaskContextDataModel.setData(resultData);
+		// 				// that.getRecordsList();
+		// 				that.busy.close();
+		// 			}
+		// 		} else {
+		// 			//			var errorText = that.oResourceModel.getText("INTERNAL_SERVER_ERROR");
+		// 			//		formatter.toastMessage(errorText);
+		// 			that.busy.close();
+		// 		}
+		// 	});
+		// 	oModel.attachRequestFailed(function (oEvent) {
+		// 		//	var errorText = that.oResourceModel.getText("INTERNAL_SERVER_ERROR");
+		// 		//	formatter.toastMessage(errorText);
+		// 		that.busy.close();
+		// 	});
+		// },
+
+		onAction: function (oEvent) {
+
+			var that = this;
+			this.busy.open();
+			this.userId = that.getOwnerComponent().getModel("oUserModel").getData().name;
+			var buttonData = oEvent.getSource().getCustomData()[0].getValue();
+			if (buttonData === "accept") {
+				var actionType = true;
+			} else {
+				var actionType = false;
+			}
+			if (this.userId === "P1940781507") {
+				var contextData = {
+					"reviewer2": {
+						"isApproved": actionType
+					}
+				};
+			} else if (this.userId === "P2000869457") {
+				var context = {
+					"reviewer1": {
+						"isApproved": actionType
+					}
+				};
+			} else {
+				var context = {
+					"approver": {
+						"isApproved": actionType
+					}
+				};
+			}
+			
+			var instanceId = that.instanceId;
+			var oUrl = "/html5apps/jabilpocui/destination/bpmworkflowruntime/workflow-service/rest/v1/xsrf-token";
+			var token = this.fetchToken(oUrl);
+			this.token = token;
+			var oPayLoad = {
+				"context": {
+					"reviewer1": {
+						"isApproved": true
+					}
+				},
+				"status": "COMPLETED",
+				"subject": "Price Update Review - P2000869457",
+				"description": "Price Update Review",
+				"recipientUsers": this.userId,
+				"recipientGroups": "string",
+				"processor": this.userId,
+				"dueDate": "2018-12-31T04:30:08.983Z",
+				"priority": "VERY_HIGH"
+			};
+			oPayLoad.context = contextData;
+
+			// var oContextData = oPayload;
+			if (this.token) {
+				var sUrl = "/html5apps/jabilpocui/destination/bpmworkflowruntime/workflow-service/rest/v1/task-instances/" + instanceId;
+				var oHeader = {
+					"Content-Type": "application/json; charset=utf-8",
+					"X-CSRF-Token": this.token
+				};
+				var oModel = new sap.ui.model.json.JSONModel();
+				oModel.loadData(sUrl, JSON.stringify(oPayLoad), true, "PATCH", false, false, oHeader);
+				oModel.attachRequestCompleted(function (oEvent) {
+					that.busy.close();
+					sap.m.MessageToast.show("Task Successfully Completed");
+				});
+				oModel.attachRequestFailed(function (oEvent) {
+					that.busy.close();
+					sap.m.MessageToast.show("Internal Server Error");
+				});
+			}
+		},
+
+		fetchToken: function (oUrl) {
+			var token;
+			$.ajax({
+				url: oUrl,
+				method: "GET",
+				async: false,
+				headers: {
+					"X-CSRF-Token": "Fetch"
+				},
+				success: function (result, xhr, data) {
+					token = data.getResponseHeader("X-CSRF-Token");
+				},
+				error: function (result, xhr, data) {
+					token = result.getResponseHeader("x-csrf-token");
+				}
+			});
+			return token;
+		},
 
 		//works only for csv files
 		// onUpload: function (e) {
